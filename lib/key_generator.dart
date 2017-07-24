351c9001ff0d029fcc79280a2750f689afc7fbfe
Honza Bittner <git@honzabittner.cz>
2017-07-24 13:29:46 +0200

    Refactor

--- lib/key_generator.dart
+++ lib/key_generator.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'package:angular2/core.dart';
 import 'package:fnx_ui/fnx_ui.dart';
 import 'package:keygenerator/generator.dart';
@@ -17,10 +18,18 @@ class KeyGenerator {
   KeyForView key;
 
   void doGenerateBlue() {
-    key = generateKey(keySize: 1024);
+    key = null;
+
+    new Future.delayed(new Duration(milliseconds: 20)).then((_) {
+      key = generateKey(keySize: 1024);
+    });
   }
 
   void doGenerateRed() {
-    key = generateKey(keySize: 2048);
+    key = null;
+
+    new Future.delayed(new Duration(milliseconds: 20)).then((_) {
+      key = generateKey(keySize: 2048);
+    });
   }
 }
